585ee7c95b43f076da9b042cb6797a1b455018a1
M Abdus Shakur <mabdusshakur12@gmail.com>
2025-04-12 00:00:22 +0600

    Add, Product

--- AdminApp/routes/web.php
+++ AdminApp/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\SliderController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SSLCommerzCredentialController;
 
@@ -16,6 +17,7 @@ Route::resource('/settings', SSLCommerzCredentialController::class);
 Route::resource('/brands', BrandController::class);
 Route::resource('/categories', CategoryController::class);
 Route::resource('/sliders', SliderController::class);
+Route::resource('/products', ProductController::class);
 
 Route::get('/login', [AuthController::class,'loginPage'])->name('login');
 Route::post('/login', [AuthController::class,'login'])->name('login.post');
