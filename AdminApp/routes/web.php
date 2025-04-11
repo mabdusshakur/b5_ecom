69fd7939b22cb2fce09d8d0816cd5f30c0688db1
M Abdus Shakur <mabdusshakur12@gmail.com>
2025-04-11 23:16:42 +0600

    Add, sliders,categories,brands routes

--- AdminApp/routes/web.php
+++ AdminApp/routes/web.php
@@ -1,13 +1,21 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SliderController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SSLCommerzCredentialController;
 
+Route::redirect("/", '/dashboard');
+
 Route::get('/dashboard', [DashboardController::class,'index'])->name('page.dashboard');
 
 Route::resource('/settings', SSLCommerzCredentialController::class);
+Route::resource('/brands', BrandController::class);
+Route::resource('/categories', CategoryController::class);
+Route::resource('/sliders', SliderController::class);
 
 Route::get('/login', [AuthController::class,'loginPage'])->name('login');
 Route::post('/login', [AuthController::class,'login'])->name('login.post');
