292ce0eb9ac7b93495c3f4ac28ed332b56ad764f
M Abdus Shakur <mabdusshakur12@gmail.com>
2025-04-13 23:47:40 +0600

    Add, auth global share

--- UserApp/app/Http/Middleware/HandleInertiaRequests.php
+++ UserApp/app/Http/Middleware/HandleInertiaRequests.php
@@ -36,9 +36,12 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
+            'auth' => [
+                'user' => fn () => $request->user() ? $request->user()->load('profile') : null,
+            ],
             'flash' => [
-                'success' => fn () => $request->session()->get('success'),
-                'error' => fn () => $request->session()->get('error'),
+                'success' => fn() => $request->session()->get('success'),
+                'error' => fn() => $request->session()->get('error'),
             ],
         ]);
     }
