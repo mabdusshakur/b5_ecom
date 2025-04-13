d3fb19193beb0a3ebc9b5d386edb196612d3574a
M Abdus Shakur <mabdusshakur12@gmail.com>
2025-04-13 22:11:00 +0600

    Fix, navbar

--- UserApp/resources/js/Pages/Shared/Navbar.vue
+++ UserApp/resources/js/Pages/Shared/Navbar.vue
@@ -1,5 +1,9 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
+import { ref } from 'vue';
+
+const userDropDown = ref(false)
+
 </script>
 
 <template>
@@ -17,7 +21,7 @@ import { Link } from '@inertiajs/vue3'
         <a href="/wishlist.html" class="text-gray-700 hover:text-blue-500">Wishlist</a>
       </div>
       <div class="relative">
-        <button id="user-dropdown-btn" class="flex items-center space-x-2">
+        <button @click="userDropDown = !userDropDown" id="user-dropdown-btn" class="flex items-center space-x-2">
           <img src="https://dummyimage.com/40x40/000/fff&text=User" alt="User" class="h-8 w-8 rounded-full" />
           <span class="text-gray-700">John Doe</span>
           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-700" fill="none" viewBox="0 0 24 24"
@@ -25,11 +29,8 @@ import { Link } from '@inertiajs/vue3'
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
           </svg>
         </button>
-        <div id="user-dropdown" class="hidden absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg">
+        <div id="user-dropdown" :class="{'hidden' : !userDropDown}" class="z-10 absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg">
           <ul class="py-2">
-            <li>
-              <a href="/profile.html" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Profile</a>
-            </li>
             <li>
               <a href="/dashboard.html" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">Dashboard</a>
             </li>
